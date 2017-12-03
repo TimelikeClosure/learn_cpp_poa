c00daba576352b85a0c2f6d6ef98e96ff71661b5
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-02 18:52:41 -0800

    added some numbers

---
+++ basic_math/math.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    short int num1, num2;
+    int sum;
+
+    num1 = 22345;
+    num2 = 5543;
+
+    sum = num1 * num2;
+
+    cout << sum << endl;
+
+    return 0;
+}
