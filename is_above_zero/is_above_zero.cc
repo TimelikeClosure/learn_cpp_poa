23f2c0940869a0da0f77557d3b247ef564dad49c
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-03 08:50:16 -0800

    checked if number is less than or equal to zero

--- is_above_zero/is_above_zero.cc
+++ is_above_zero/is_above_zero.cc
@@ -4,12 +4,16 @@ using namespace std;
 int main(void){
     int theNumber;
 
-    theNumber = 2334;
+    theNumber = -6;
 
     cout << endl << "theNumber = " << theNumber << endl;
 
     if (theNumber > 0){
         cout << "theNumber is greater than zero" << endl;
+    } else if (theNumber < 0){
+        cout << "theNumber is less than zero" << endl;
+    } else{
+        cout << "theNumber is equal to zero" << endl;
     }
 
     return 0;
