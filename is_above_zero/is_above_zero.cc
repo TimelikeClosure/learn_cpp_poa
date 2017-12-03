ecb35aa976292d41abc46536007a2efc40c5a339
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-03 08:42:51 -0800

    checked if number is greater than zero

---
+++ is_above_zero/is_above_zero.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int theNumber;
+
+    theNumber = 2334;
+
+    cout << endl << "theNumber = " << theNumber << endl;
+
+    if (theNumber > 0){
+        cout << "theNumber is greater than zero" << endl;
+    }
+
+    return 0;
+}
